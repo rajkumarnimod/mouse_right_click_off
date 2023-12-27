336c7a1454ee1bb3ff82375ca6d75fe6ee1067c8
rajkumarnimod <130267707+rajkumarnimod@users.noreply.github.com>
2023-12-27 14:05:59 +0530

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -1,3 +1,10 @@
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
 });
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+        e.preventDefault();
+    }
+});
